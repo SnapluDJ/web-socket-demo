215dca22b5f2f86b75ad110088d6fd0a8863bd99
dongjie.lu <dongjie.lu@SH22001ML.local>
2021-03-26 14:29:56 +0800

    feat: use single server for both http & ws

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const WebSocket = require('ws');
+const express = require('express');
+const path = require('path');
 
-const wss = new WebSocket.Server({
-  port: 3000,
-});
+const app = express();
+app.use('/', express.static(path.resolve(__dirname, '../client')));
+const server = app.listen(3000);
+
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
   ws.onmessage = ({ data }) => {
