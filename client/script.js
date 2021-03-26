a5667a28ad08fb7a8562e72d186886ebed4c6fa7
dongjie.lu <dongjie.lu@SH22001ML.local>
2021-03-26 11:52:43 +0800

    basic use of ws in both client & server

--- client/script.js
+++ client/script.js
@@ -1,1 +1,41 @@
-// const ws = new WebSocket('wss://localhost:3001');
+const ws = new WebSocket('ws://localhost:3000/webSocket');
+
+const message = document.getElementById('messages');
+const input = document.getElementById('message');
+const button = document.getElementById('send');
+const body = document.querySelector('body');
+
+button.disabled = true;
+
+const sendMessage = () => {
+  ws.send(input.value);
+};
+
+const generateNewMessage = (data) => {
+  const newMessage = document.createElement('div');
+  newMessage.innerText = `server echo: ${data}`;
+
+  message.appendChild(newMessage);
+};
+
+button.addEventListener('click', sendMessage, false);
+body.addEventListener(
+  'keydown',
+  (e) => {
+    if (e.code === 'Enter') {
+      sendMessage();
+    }
+  },
+  false
+);
+
+ws.onopen = (e) => {
+  button.disabled = false;
+  if (input.value) {
+    sendMessage();
+  }
+};
+
+ws.onmessage = ({ data }) => {
+  generateNewMessage(data);
+};
--- server/index.js
+++ server/index.js
@@ -1,19 +1,12 @@
-// const express = require('express');
-
-// const app = express();
-
-// const PORT = 3000;
-
-// app.listen(PORT, () => {
-//   console.log(`server is listening port ${PORT}`);
-// });
-
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({
   port: 3000,
 });
 
-wss.on('open', () => wss.send('connection is open'));
-
-// wss.on('connection', (ws) => {});
+wss.on('connection', (ws) => {
+  ws.onmessage = ({ data }) => {
+    console.log(data);
+    ws.send(data);
+  };
+});
